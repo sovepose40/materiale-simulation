fb506c1d5cfe84365ea7447efcb12696b52b848f
sovepose40 <adamkpersson@gmail.com>
2026-04-14 18:48:04 +0200

    sand and simulation class added - man kan trykke på grids for at spawne statisk sand.

--- materiale simulation.py
+++ materiale simulation.py
@@ -1,4 +1,4 @@
-import pygame,sys
+import pygame,sys,random,colorsys
 
 #starter pygame
 pygame.init()
@@ -28,9 +28,52 @@ class Grid:
     def draw(self,window):
         for row in range(self.rows):
             for col in range(self.cols):
-                pygame.draw.rect(window, light_grey, (col * self.node_size, row * self.node_size, self.node_size-1, self.node_size-1))
-grid = Grid(window_width, window_height, node_size)
+                color = light_grey
+                particle = self.nodes[row][col]
+                if particle is not None:
+                    color = particle.color
+                pygame.draw.rect(window, color, (col * self.node_size, row * self.node_size, self.node_size-5, self.node_size-5))
+    
+    def add_particle(self, row, col, particle_type):
+        if 0 <= row < self.rows and 0 <= col < self.cols:
+            self.nodes[row][col] = particle_type()
 
+    def remove_particle(self, row, col):
+        if 0 <= row < self.rows and 0 <= col < self.cols:
+            self.nodes[row][col] = None
+
+
+class sand:
+    def __init__(self):
+        self.color = self.random_color()
+
+    def random_color(self):
+        hue = random.uniform(0.1,0.12)
+        saturation = random.uniform(0.5,0.7)
+        value = random.uniform(0.6,0.8)
+        r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
+        return int(r * 255), int(g * 255), int(b * 255)
+    
+        
+
+class Simulation:
+    def __init__(self,width,height,node_size):
+        self.grid = Grid(width, height, node_size)
+
+    def draw(self,window):
+        self.grid.draw(window)
+    def add_particle(self, row, col):
+        self.grid.add_particle(row, col, sand)
+    def remove_particle(self, row, col):
+        self.grid.remove_particle(row, col)
+
+simulation = Simulation(window_width, window_height, node_size)
+
+simulation.add_particle(0,0)
+simulation.add_particle(1,1)
+
+
+simulation.remove_particle(0,0)
 
 
 #main loop
@@ -42,12 +85,16 @@ while True:
             pygame.quit()
             sys.exit()
 
-    #update stuff
+    buttons = pygame.mouse.get_pressed()
+    if buttons[0]:
+        pos = pygame.mouse.get_pos()
+        row = pos[1] // node_size
+        col = pos[0] // node_size
+        simulation.add_particle(row, col)
 
     #draw stuff
     window.fill(grey)
-    grid.draw(window)   
-
+    simulation.draw(window)
 
     # opdaterer displayet og sætter fps
     pygame.display.flip()
